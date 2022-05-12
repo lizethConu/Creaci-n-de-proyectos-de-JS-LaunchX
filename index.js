5cd4225aa496c6adc3d605635d095032d1fa0c98
Lizeth Covarrubias <47550110+lizethConu@users.noreply.github.com>
2022-05-11 21:48:04 -0600

    Actualización de Ejercicios

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const karen = new MissionCommander("Karen")
+const karla = new MissionCommander("Karla")
+const carlos = new MissionCommander("Carlos")
+console.log(karen.name)
+console.log(karla.name)
+console.log(carlos.name)
